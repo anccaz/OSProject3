5d92b368bfbe997d5596868b27865b1d73ec510e
Annie Li <108150567+anccaz@users.noreply.github.com>
2026-04-28 12:32:46 -0500

    Header layout created

--- project3.py
+++ project3.py
@@ -17,3 +17,8 @@ MAGIC           = b"4348PRJ3"
 MIN_DEGREE      = 10                   # t = 10  →  max 19 keys, 20 children
 MAX_KEYS        = 2 * MIN_DEGREE - 1   # 19
 MAX_CHILDREN    = 2 * MIN_DEGREE       # 20
+
+# Header layout  (all offsets within block 0)
+HDR_MAGIC_OFF   = 0
+HDR_ROOT_OFF    = 8
+HDR_NEXT_OFF    = 16
